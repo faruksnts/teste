73d06de9472b8898531d4a35161e156e489aa878
snts <142832162+faruksnts@users.noreply.github.com>
2023-10-20 13:37:44 -0400

    metoo.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import streamlit as st
-import webbrowser
 
 def main():
-    st.title("Oi, amiga!")
+    st.title("Oi, amor!")
     st.write("Aceita ser minha pessoa favorita para sempre?")
-
+    
     resposta = st.button("Sim")
     if resposta:
         st.write("Sabia que você aceitaria! 😊")
         # Altere o URL abaixo para o endereço da música que você deseja compartilhar.
-        webbrowser.open('https://youtu.be/238I_qBI4Rs?si=8PD797DQ_sA5U8i7')
+        url = 'https://youtu.be/238I_qBI4Rs?si=8PD797DQ_sA5U8i7'
+        st.markdown(f"[estou com sdds, clique aqui!]({url})")
 
 if __name__ == "__main__":
     main()
+
 
 
 
