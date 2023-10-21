48e6885a240ed821a9405a27ef70a6c1144ea900
snts <142832162+faruksnts@users.noreply.github.com>
2023-10-21 11:28:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def main():
     st.title("Oi, amor!")
     st.write("Lembra daquela vez que te ajudei ?? Então, será que você poderia pagar meu Uber hoje?")
     
-    resposta = st.button("Claro!")
+    resposta = st.button("Claro! (ps: tu não tem escolha, pode clicar!")
     if resposta:
         st.write("Você é 10!")
         # Aqui, podemos colocar um link para uma música ou mensagem de agradecimento, por exemplo.
