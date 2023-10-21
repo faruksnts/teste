d65510b4962507218d818613a058f344a02193d6
snts <142832162+faruksnts@users.noreply.github.com>
2023-10-21 11:23:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
 import streamlit as st
-import random
 
 def main():
-    st.title("Pedra, Papel e Tesoura!")
-    st.write("Vamos jogar!")
-
-    opcoes = ["Pedra", "Papel", "Tesoura"]
-    selecao = st.selectbox("Escolha sua jogada:", opcoes)
-
-    if st.button("Jogar!"):
-        jogada_computador = random.choice(opcoes)
-        st.write(f"Computador escolheu: {jogada_computador}")
-
-        if selecao == jogada_computador:
-            st.write("Empate!")
-        elif (selecao == "Pedra" and jogada_computador == "Tesoura") or \
-             (selecao == "Papel" and jogada_computador == "Pedra") or \
-             (selecao == "Tesoura" and jogada_computador == "Papel"):
-            st.write("Você ganhou!")
-        else:
-            st.write("Você perdeu!")
+    st.title("Oi, amor!")
+    st.write("Lembra daquela vez que te ajudei ?? Então, será que você poderia pagar meu Uber hoje?")
+    
+    resposta = st.button("Claro!")
+    if resposta:
+        st.write("Você é 10!")
+        # Aqui, podemos colocar um link para uma música ou mensagem de agradecimento, por exemplo.
+        url = 'https://www.instagram.com/reel/CvwxfEWOLIv/?igshid=MzRlODBiNWFlZA=='
+        st.markdown(f"[Clique aqui! A pessoa mais linda da familia! 😊]({url})")
 
 if __name__ == "__main__":
     main()
